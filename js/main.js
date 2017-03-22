86f15db76de89eee347a1ac536609733bd22629d
schlagercollin <schlagercollin@gmail.com>
2017-03-22 13:32:32 -0600

    More mobile styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+  $("#close-button").click(function() {
+    $("#main-nav").removeClass("active");
+    $(".title-text,body").removeClass("body-active");
+    $("#nav-mask").removeClass("mask-active");
+  });
+  $("#open-button").click(function() {
+    $("#main-nav").addClass("active");
+    $(".title-text,body").addClass("body-active");
+    $("#nav-mask").addClass("mask-active");
+  });
+  $("#nav-mask").click(function() {
+    $("#main-nav").removeClass("active");
+    $(".title-text,body").removeClass("body-active");
+    $("#nav-mask").removeClass("mask-active");
+  })
 });
